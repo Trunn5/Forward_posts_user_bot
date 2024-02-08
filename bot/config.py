63cb7dd587ae15c3782bddd899e693dd9e6aa715
Dmitry5567 <prosvirkin.dmitry@gmail.com>
2024-02-09 00:35:58 +0300

    fix айди в конфиге

--- bot/config.py
+++ bot/config.py
@@ -6,14 +6,14 @@ API_HASH_1 = os.getenv("API_HASH_1")
 API_ID_2 = os.getenv("API_ID_2")
 API_HASH_2 = os.getenv("API_HASH_2")
 
-admins: list[int] = [473175291]
+admins: list[int] = []
 
 # Интвервал спама постов (в секундах)
 spam_interval = 10
 
 
-# К группам и каналам добавлять в начале -100
-# Например: 123456 -> -100123456
+# айди канала/группы берем из веб версии тг
+# или с помощью @username_to_id_bot
 
 # Канал по аренде
 rent_channel_id = -1001617975958
